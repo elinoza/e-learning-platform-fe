cae72fae61ec843108ed816d564a3e97fe9dd105
elinoza <hillcakmak@gmail.com>
2021-03-14 14:41:53 +0300

    recommandation carousel issues

--- e-larning-platform/src/components/Home/Home.jsx
+++ e-larning-platform/src/components/Home/Home.jsx
@@ -5,28 +5,35 @@ import {
   Container,
   Form,
   Button,
-  Carousel,
+  
   Badge,
-  Tabs,Tab
+  Tabs,
+  Tab,
+Card
 } from "react-bootstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
 import { GiTrophyCup } from "react-icons/gi";
+
+ import Recommended from "./Recommended"
 import { Avatar } from "@material-ui/core";
 import { Line, Circle } from "rc-progress";
-// import { AiFillLinkedin} from 'react-icons/ai';
-// import downloadAppStore from "../assets/downloadAPPstore.png";
-// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-// import logo from "../assets/Instagram-Logo.png";
-// import {FcGoogle  } from "react-icons/fc";
-// import Footer from "./Footer"
+import responsive from "./Carousel"
+
 import "./home.css";
 
 class Home extends Component {
-  state = {};
+  state = {
+
+  };
+
+
 
   render() {
     return (
       <>
-        <Carousel>
+        {/* <Carousel>
           <Carousel.Item className="carousel-item m-3">
             <img
               className="d-block w-100 bg-image"
@@ -87,12 +94,12 @@ class Home extends Component {
               </div>
             </Carousel.Caption>
           </Carousel.Item>
-        </Carousel>
+        </Carousel> */}
 
         <Container>
           <Row className=" border-bottom mt-5 ">
             <Col xs={12} md={6} className="  p-3">
-              <h2 style={{fontSize:"18px"}}>Set Weekly Goal</h2>
+              <h2 style={{ fontSize: "18px" }}>Set Weekly Goal</h2>
               <div className="d-flex flex-row justify-content-between border-top mt-4 pt-2">
                 <div className="goal-parent m-0  ">
                   <Circle
@@ -114,94 +121,121 @@ class Home extends Component {
                       border: "1px solid #0973B1",
                     }}
                   >
-                    <span style={{ color: "#0973B1", fontWeight:"bold" }}>Set a goal</span>
+                    <span style={{ color: "#0973B1", fontWeight: "bold" }}>
+                      Set a goal
+                    </span>
                   </Button>
                 </div>
               </div>
             </Col>
             <Col xs={12} md={6} className=" myprogress-home p-3 ">
-            <Tabs defaultActiveKey="progress" id="myprogresss-tab" className="mt-2">
-  <Tab eventKey="progress" title="In Progress" >
-  <div className="d-flex  border-top pt-2">
-                {" "}
-                <div className="d-flex">
-                  
-                <div
-                  className="d-inline border mr-2"
-                  style={{ width: "98px", height: "55px" }}
-                >
-                  <img
-                    className="d-block w-100 "
-                    src="https://mysominotes.files.wordpress.com/2017/07/powerpointlecture.jpg"
-                    className="w-100"
-                    alt="progress video"
-                  />
-                </div>
-                <div className="d-inline">
-                  <p
-                    style={{ fontSize: "14px", fontWeight: "bold" }}
-                    className="mb-0"
-                  >
-                    Code Challenges: JavaScript
-                  </p>
-                  <p
-                    style={{ fontSize: "12px", fontWeight: "bold" }}
-                    className="text-muted mb-0"
-                  >
-                    COURSE . 48 m 48 s left
-                  </p>
-                  <Line percent="10" strokeWidth="1.5" strokeColor="#0573B1" />
-                </div>
-              </div>
-              <div className="ml-auto d-inline" style={{color:"#554AC2"}}><a>Show All (1) </a></div>
-              </div>
-             
-           
-  </Tab>
-  <Tab eventKey="saved" title="Saved">
-  <div className="d-flex  border-top pt-2">
-                {" "}
-                <div  className="d-flex" >
-                <div
-                  className="d-inline border mr-2"
-                  style={{ width: "98px", height: "55px" }}
-                >
-                  <img
-                    className="d-block w-100 "
-                    src="https://mysominotes.files.wordpress.com/2017/07/powerpointlecture.jpg"
-                    className="w-100"
-                    alt="progress video"
-                  />
-                </div>
-                <div className="d-inline">
-                  <p
-                    style={{ fontSize: "14px", fontWeight: "bold" }}
-                    className="mb-0"
-                  >
-                    Code Challenges: JavaScript
-                  </p>
-                  <p
-                    style={{ fontSize: "12px", fontWeight: "bold" }}
-                    className="text-muted mb-0"
-                  >
-                    COURSE . 48 m 48 s left
-                  </p>
-                  <Line percent="10" strokeWidth="1.5" strokeColor="#0573B1" />
-                </div>
-                </div>
-                
-                <div className="ml-auto d-inline" style={{color:"#554AC2"}}><a>Show All (1) </a></div>
-              </div>
-           
- 
-  </Tab>
-  
-</Tabs>
-            
-             
-
+              <Tabs
+                defaultActiveKey="progress"
+                id="myprogresss-tab"
+                className="mt-2"
+              >
+                <Tab eventKey="progress" title="In Progress">
+                  <div className="d-flex  border-top pt-2">
+                    {" "}
+                    <div className="d-flex">
+                      <div
+                        className="d-inline border mr-2"
+                        style={{ width: "98px", height: "55px" }}
+                      >
+                        <img
+                          className="d-block w-100 "
+                          src="https://mysominotes.files.wordpress.com/2017/07/powerpointlecture.jpg"
+                          className="w-100"
+                          alt="progress video"
+                        />
+                      </div>
+                      <div className="d-inline">
+                        <p
+                          style={{ fontSize: "14px", fontWeight: "bold" }}
+                          className="mb-0"
+                        >
+                          Code Challenges: JavaScript
+                        </p>
+                        <p
+                          style={{ fontSize: "12px", fontWeight: "bold" }}
+                          className="text-muted mb-0"
+                        >
+                          COURSE . 48 m 48 s left
+                        </p>
+                        <Line
+                          percent="10"
+                          strokeWidth="1.5"
+                          strokeColor="#0573B1"
+                        />
+                      </div>
+                    </div>
+                    <div
+                      className="ml-auto d-inline"
+                      style={{ color: "#554AC2" }}
+                    >
+                      <a>Show All (1) </a>
+                    </div>
+                  </div>
+                </Tab>
+                <Tab eventKey="saved" title="Saved">
+                  <div className="d-flex  border-top pt-2">
+                    {" "}
+                    <div className="d-flex">
+                      <div
+                        className="d-inline border mr-2"
+                        style={{ width: "98px", height: "55px" }}
+                      >
+                        <img
+                          className="d-block w-100 "
+                          src="https://mysominotes.files.wordpress.com/2017/07/powerpointlecture.jpg"
+                          className="w-100"
+                          alt="progress video"
+                        />
+                      </div>
+                      <div className="d-inline">
+                        <p
+                          style={{ fontSize: "14px", fontWeight: "bold" }}
+                          className="mb-0"
+                        >
+                          Code Challenges: JavaScript
+                        </p>
+                        <p
+                          style={{ fontSize: "12px", fontWeight: "bold" }}
+                          className="text-muted mb-0"
+                        >
+                          COURSE . 48 m 48 s left
+                        </p>
+                        <Line
+                          percent="10"
+                          strokeWidth="1.5"
+                          strokeColor="#0573B1"
+                        />
+                      </div>
+                    </div>
+                    <div
+                      className="ml-auto d-inline"
+                      style={{ color: "#554AC2" }}
+                    >
+                      <a>Show All (1) </a>
+                    </div>
+                  </div>
+                </Tab>
+              </Tabs>
             </Col>
           </Row>
+          <div className="my-5 ">
+            <h4>Top Picks For Hilal</h4>
+            <MultipleCarousel responsive={responsive}>
+            <Row >
+            <Recommended />
+           
+          </Row>
+          
+</MultipleCarousel>
+          
+       
+        
+          </div>
         </Container>
       </>
     );
---
+++ e-larning-platform/src/components/Home/Recommended.jsx
@@ -0,0 +1,125 @@
+
+
+import React, { Component } from "react";
+import {
+
+  Col,
+  Container,
+
+  Button,
+  Carousel,
+
+Card
+} from "react-bootstrap";
+
+import { BsPlay ,BsBookmark} from "react-icons/bs";
+
+import { Avatar } from "@material-ui/core";
+
+
+
+import "./home.css";
+
+class Recommended extends Component {
+  state = {
+    showDetail:false
+  };
+  handleHover=()=>{
+    this.setState({showDetail:!this.state.showDetail})
+  }
+  render() {
+    return (
+      <>
+     
+     <Col>
+<Card className="recommendation-card" style={{ width: '18rem' }} onMouseEnter={
+      this.handleHover
+     } onMouseLeave={
+   this.handleHover
+     }>
+<Card.Img variant="top" src="https://mysominotes.files.wordpress.com/2017/07/powerpointlecture.jpg" className="border"/>
+<p
+              style={{ fontSize: "12px", fontWeight: "bold" }}
+              className="text-muted mb-1 "
+            >
+
+             <BsPlay style={{fontSize:"16px"}}/>  COURSE 
+            </p>
+<Card.Body>
+
+<Card.Text>
+Code Challenges: JavaScript
+<p
+              style={{ fontSize: "14px"}}
+              className={this.state.showDetail === true ? "d-block text-muted mt-1 mb-0" :"d-none"} 
+            >
+
+            By: Christina Gordon
+            </p>
+            <div className= {this.state.showDetail === true ? "d-block" :"d-none"} 
+           
+   >
+
+            <p
+              style={{ fontSize: "12px", fontWeight: "bold" }}
+              className=" mb-0"
+            >
+              Intermediate . 48 m 48 s left .Dec/1 2020
+            </p>
+            <div className="d-flex my-2">
+      <Avatar
+        src="https://upload.wikimedia.org/wikipedia/en/d/d7/Random_person_image.png"
+        className="navbar-logo m-0 mt-1 p-0 d-inline"
+      />
+      <div>
+        {" "}
+        <p
+          style={{ fontSize: "12px" }}
+          className="d-block ml-1 mb-0 p-0 "
+        >
+          Dorie Clark
+        </p>
+        <p
+          style={{ fontSize: "12px" }}
+          className="d-block ml-1 mb-0 p-0 "
+        >
+          Professor of Something
+        </p>
+      </div>
+    </div>
+
+    <p
+              style={{ fontSize: "12px"}}
+              className="text-muted mt-1 mb-0"
+            >
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam rem eveniet culpa enim a nostrum. Natus accusantium recusandae labore accusamus, voluptatum quas mollitia dolorem quos consectetur dolor est molestias.
+            </p>
+            <div className="d-flex">
+            <p
+              style={{ fontSize: "12px",fontWeight:"Bold"}}
+              className="text-muted mt-2 mb-0"
+            >
+
+           1.787 Learners
+            </p>
+
+<Button className="SaveButton ml-auto"  size="sm"><BsBookmark/>
+Save
+</Button>
+            </div>
+
+            </div>
+           
+
+</Card.Text>
+
+</Card.Body>
+</Card>
+</Col>
+        
+      </>
+    );
+  }
+}
+export default Recommended;
