fe20d6f0f2124f252a045d151b6dbc4e4d117cd6
elinoza <hillcakmak@gmail.com>
2021-03-19 18:39:12 +0300

    ..

--- e-larning-platform/src/components/Learn/Learn.jsx
+++ e-larning-platform/src/components/Learn/Learn.jsx
@@ -4,18 +4,17 @@ import {
   Col,
   Container,
   Tabs,
-  Navbar,Form,Button,NavDropdown,Nav,FormControl,
+ 
   Tab,
 } from "react-bootstrap";
 
 import Notes from "./Notes"
 import Comments from "./Comments"
 import OverView from "./overView"
+import videoNavbar from "./videoNavbar"
 import Videojs  from "./Video.js"
-import videoNavbar from "./videoNavbar.js"
-import {AiOutlineUnorderedList,AiOutlineLike,} from 'react-icons/ai';
-import {RiShareForwardLine} from 'react-icons/ri';
-import {BsBookmark} from 'react-icons/bs';
+
+
 import "./Learn.css";
 
 
@@ -46,49 +45,24 @@ class Learn extends Component {
   render() {
     return (
       <>
-      <Row>
-        <Col  md={3}>
+     
       <div class="sidebar">
       <a class="active" href="#home">Home</a>
       <a href="#news">News</a>
       <a href="#contact">Contact</a>
       <a href="#about">About</a>
     </div>
-    </Col>
-  <Col md={9}  >
-        <Row className="mx-0">
-          <Col xs={12} className="player-col">
-            <img src="https://annaabi.ee/thumb/a56708d3418b83a933dedea9d731fe26.jpg"/>
-         
+   
+
+        <Row className="Main">
+          <Col xs={12} className="player-col p-0">
+          
+         <videoNavbar/>
           {/* <Videojs {...videoJsOptions} /> */}
-          <Navbar id="videoNavbar" className="  py-1 " expand="lg">
-  <Navbar.Brand className=" pr-1 sidebar-toggle-wrap">
-  <Button className="sidebar-toggle-Button  "  size="sm"> <AiOutlineUnorderedList  className="mr-2"/>
-  Contents
-</Button>
-    {/* <p style={{color:"#aeaeaf",display:"inline", fontSize:"18px",fontWeight:"bold"}}>Contents</p> */}
-  </Navbar.Brand>
-
-
-  <div className="mr-auto ">
-    <p style={{fontWeight:"bold"}}>Course Name</p>
-    <p style={{color:"#aeaeaf"}}>Content Name</p>
-  </div>
- 
-    <div className="ml-auto icons ">
-    <AiOutlineLike className="icons mr-2 ml-2"/>
-    474
-   
-    <BsBookmark  className="icons mr-2 ml-2"/>
-    345
-    <RiShareForwardLine   className="icons mr-2 ml-2"/>
-    
-    </div>
-   
-
-</Navbar>
+         
+          
           </Col>
-          <Col xs={12}>
+          <Col xs={12} className="p-0">
             <div className="learn">
               <Container>
                 <Tabs
@@ -113,8 +87,7 @@ class Learn extends Component {
             </div>
           </Col>
         </Row>
-        </Col>
-        </Row>
+      
       </>
     );
   }
--- e-larning-platform/src/components/Learn/videoNavbar.js
+++ e-larning-platform/src/components/Learn/videoNavbar.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
 import {
-    Navbar,Form,Button,NavDropdown,Nav,FormControl
-
+    Navbar,Button
 } from "react-bootstrap";
+import {AiOutlineUnorderedList,AiOutlineLike,} from 'react-icons/ai';
+import {RiShareForwardLine} from 'react-icons/ri';
+import {BsBookmark} from 'react-icons/bs';
 
 import "./Learn.css";
-
-
-
-
-
-
 
 class videoNavbar extends Component {
   state = {};
@@ -18,28 +14,29 @@ class videoNavbar extends Component {
   render() {
     return (
       <>
-       <Navbar id="videoNavbar" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>
-  </Navbar.Collapse>
-</Navbar>
-      
+        <Navbar id="videoNavbar" className="  py-1 " expand="lg">
+          <Navbar.Brand className=" pr-1 sidebar-toggle-wrap">
+            <Button className="sidebar-toggle-Button  " size="sm">
+              {" "}
+              <AiOutlineUnorderedList className="mr-2" />
+              Contents
+            </Button>
+            {/* <p style={{color:"#aeaeaf",display:"inline", fontSize:"18px",fontWeight:"bold"}}>Contents</p> */}
+          </Navbar.Brand>
+
+          <div className="mr-auto ">
+            <p style={{ fontWeight: "bold" }}>Course Name</p>
+            <p style={{ color: "#aeaeaf" }}>Content Name</p>
+          </div>
+
+          <div className="ml-auto icons ">
+            <AiOutlineLike className="icons mr-2 ml-2" />
+            474
+            <BsBookmark className="icons mr-2 ml-2" />
+            345
+            <RiShareForwardLine className="icons mr-2 ml-2" />
+          </div>
+        </Navbar>
       </>
     );
   }
